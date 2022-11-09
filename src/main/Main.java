5570c00576d706cbf923b5035e8f82a31a4e86b8
leedongha1998 <78835939+leedongha1998@users.noreply.github.com>
2022-11-09 15:32:47 +0900

    초기 설정

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,7 @@
-package atm_project;
+package main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world~~");
 	}
 }
