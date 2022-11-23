ea86204dcee9da96e2d06576fd89c6c5717e1620
shlim0287 <112301522+shlim0287@users.noreply.github.com>
2022-11-23 20:23:33 +0900

    최신화2

--- src/main/Hana.java
+++ src/main/Hana.java
@@ -2,6 +2,14 @@ package main;
 
 public class Hana extends Bank {
 
- 
+	public Hana() {
+		super();
+	}
+
+	public Hana(String bankName, long balance, long deposit, long withdrawal) {
+		super(bankName, balance, deposit, withdrawal);
+	}
+
+	
  
 }
