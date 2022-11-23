eca9c4c5be0368c2331b4a0a33892427878e3a49
shlim0287 <112301522+shlim0287@users.noreply.github.com>
2022-11-23 20:23:00 +0900

    11/23 최신화

--- src/main/Bank.java
+++ src/main/Bank.java
@@ -5,41 +5,53 @@ public class Bank {
 	private long balance; //잔액
 	private long deposit;//입금액
 	private long withdrawal;//출금액
-	
+	private double commission;//수수료
+
 	public Bank() {
+		super();
 		
 	}
-	public Bank(String bankName,long balance,long deposit,long withdrawal) {
-		this.bankName=bankName;
-		this.balance=balance;
-		this.deposit=deposit;
-		this.withdrawal=withdrawal;
+	public Bank(String bankName, long balance, long deposit, long withdrawal) {
+		super();
+		this.bankName = bankName;
+		this.balance = balance;
+		this.deposit = deposit;
+		this.withdrawal = withdrawal;
+		
 	}
-	
 	public String getBankName() {
 		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
 	}
 	public long getBalance() {
 		return balance;
 	}
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
 	public long getDeposit() {
 		return deposit;
+	}
+	public void setDeposit(long deposit) {
+		this.deposit = deposit;
 	}
 	public long getWithdrawal() {
 		return withdrawal;
 	}
-	public void setBankName(String bankName) {
-		this.bankName=bankName;
-	}
-	public void setBalance(long balance) {
-		this.balance=balance;
-	}
-	public void setDeposit(long deposit) {
-		this.deposit=deposit;
-	}
 	public void setWithdrawal(long withdrawal) {
-		this.withdrawal=withdrawal;
+		this.withdrawal = withdrawal;
 	}
+
+
+	@Override
+	public String toString() {
+		return "Bank [bankName=" + bankName + ", balance=" + balance + ", deposit=" + deposit + ", withdrawal="
+				+ withdrawal + ", accountNumber="  + "]";
+	}
+	
+
 	
 	
 } 
