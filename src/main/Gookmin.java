6a47fc823adde536eb82cf5b774109355045d8d9
shlim0287 <112301522+shlim0287@users.noreply.github.com>
2022-11-24 15:07:10 +0900

    11/24 최신화

--- src/main/Gookmin.java
+++ src/main/Gookmin.java
@@ -2,4 +2,33 @@ package main;
 
 public class Gookmin extends Bank {
 
+	private double commission=0.3;
+
+	public Gookmin() {
+		super();
+
+	}
+
+	public Gookmin(String bankName, long balance, long deposit, long withdrawal) {
+		super(bankName, balance, deposit, withdrawal);
+
+	}
+	public Gookmin(String bankName, long balance, long deposit, long withdrawal, double commission) {
+		super(bankName, balance, deposit, withdrawal);
+		this.commission = commission;
+	}
+	
+
+	public double getCommission() {
+		return commission;
+	}
+
+	@Override
+	public String toString() {
+		return "Gookmin [commission=" + commission + ", getBankName()=" + getBankName() + ", getBalance()="
+				+ getBalance() + ", getDeposit()=" + getDeposit() + ", getWithdrawal()=" + getWithdrawal()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
 }
--- src/main/Sinhan.java
+++ src/main/Sinhan.java
@@ -1,5 +1,34 @@
 package main;
 
 public class Sinhan extends Bank{
+	private double commission=0.4;
 
+	public Sinhan() {
+		super();
+	
+	}
+
+	public Sinhan(String bankName, long balance, long deposit, long withdrawal) {
+		super(bankName, balance, deposit, withdrawal);
+	
+	}
+
+	public Sinhan(String bankName, long balance, long deposit, long withdrawal, double commission) {
+		super(bankName, balance, deposit, withdrawal);
+		this.commission = commission;
+	}
+	
+
+	public double getCommission() {
+		return commission;
+	}
+
+	@Override
+	public String toString() {
+		return "Sinhan [commission=" + commission + ", getBankName()=" + getBankName() + ", getBalance()="
+				+ getBalance() + ", getDeposit()=" + getDeposit() + ", getWithdrawal()=" + getWithdrawal()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
 }
